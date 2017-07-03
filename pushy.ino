221f0188e79aabb95f7e37b5e58d6bdc4b9a6fd1
Iman Fayek <ifayek@ucsc.edu>
2017-07-03 16:30:28 -0700

    Added L & R

--- pushy.ino
+++ pushy.ino
@@ -1,34 +1,63 @@
 #include <Adafruit_NeoPixel.h>
-
 Adafruit_NeoPixel strand( 120, 7, NEO_GRB+NEO_KHZ800);
-int sensorValue;
-int normalValue=0;
-int topPixel = 0;
+int sensorValueL;
+int sensorValueR;
+
+int normalValueL = 0;
+int normalValueR = 0;
+
+int leftPixel = 0;
+int rightPixel = 0;
+
 bool flashOn = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(3,INPUT);
-  pinMode(7,OUTPUT);
+  pinMode(3,INPUT); // button L
+  pinMode(4, INPUT); // button R
+  pinMode(7,OUTPUT); // lights
   strand.begin();
 }
 
 void loop() {
 
-  sensorValue = analogRead(A0);
+  // read sensor input
+  sensorValueL = analogRead(A0);
+  sensorValueR = analogRead(A1);
 
+  // left button is pressed, set sensor at left end
   if( digitalRead( 3 ) == LOW ) {
-    normalValue = sensorValue;
-    topPixel = 0;
+    normalValueL = sensorValueL;
+    leftPixel = 0;
   }
+
+  // right button is pressed, set sensor at right end
+  if( digitalRead(4) == LOW) {
+    normalValueR = sensorValueR;
+    rightPixel = 119;
+  }
+  
   Serial.print(sensorValue-normalValue);
   Serial.print(", ");
 
-  if( normalValue == 0 ) { // button hasn't been pressed yet.
-    if( flashOn == 0 ) { strand.setPixelColor( 0, 0, 0, 255 ); flashOn = 1; }
-    else { strand.setPixelColor( 0, 0, 0, 0 ); flashOn = 0; }
+  // buttons have not been pressed, blink light at each end
+  if( normalValueL == 0  && normalValueR ==0) 
+  { 
+    if( flashOn == 0 ) 
+    { 
+      strand.setPixelColor( 0, 0, 0, 255 ); 
+      strand.setPixelColor( 119, 0, 0, 255);
+      flashOn = 1; 
+    }
+    else 
+    { 
+      strand.setPixelColor( 0, 0, 0, 0 ); 
+      strand.setPixelColor(119, 0, 0, 0);
+      flashOn = 0; 
+    }
   }
   else {
+    // Pixel update
     for( int i=0; i< 120; i++ ) {
       if( i > topPixel ) { strand.setPixelColor(i, 0, 0, 0); }
       else { strand.setPixelColor( i, 255, 0, 0 ); }
